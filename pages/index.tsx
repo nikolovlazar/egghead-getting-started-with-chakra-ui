a0217cc6e79f0ae3dfd09ff4ea99fc6b9bb26b71
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-08-23 08:14:23 +0200

    Lesson 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,25 @@
-const IndexPage = () => <h1>Hello Next.js 👋</h1>;
+import { Flex, Container, VStack } from '@chakra-ui/react';
+
+const IndexPage = () => (
+  <Container maxW="container.xl" p={0}>
+    <Flex h="100vh" py={20}>
+      <VStack
+        w="full"
+        h="full"
+        p={10}
+        spacing={10}
+        alignItems="flex-start"
+      ></VStack>
+      <VStack
+        w="full"
+        h="full"
+        p={10}
+        spacing={10}
+        alignItems="flex-start"
+        bg="gray.50"
+      ></VStack>
+    </Flex>
+  </Container>
+);
 
 export default IndexPage;
