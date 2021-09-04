b9fb3ed4ecf3cfbbed7a17b266e44af0e1850038
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-09-04 20:41:09 +0200

    lesson 5

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ import Details from '../src/sections/details';
 
 const IndexPage = () => (
   <Container maxW="container.xl" p={0}>
-    <Flex h="100vh" py={20}>
+    <Flex
+      h={{ base: 'auto', md: '100vh' }}
+      py={[0, 10, 20]}
+      direction={{ base: 'column-reverse', md: 'row' }}
+    >
       <Details />
       <Cart />
     </Flex>
--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -10,9 +10,11 @@ import {
   Select,
   Checkbox,
   Button,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 
 const Details = () => {
+  const colSpan = useBreakpointValue({ base: 2, md: 1 });
   return (
     <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
       <VStack spacing={3} alignItems="flex-start">
@@ -20,13 +22,13 @@ const Details = () => {
         <Text>If you already have an account, click here to log in.</Text>
       </VStack>
       <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>First Name</FormLabel>
             <Input placeholder="John" />
           </FormControl>
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>Last Name</FormLabel>
             <Input placeholder="Doe" />
@@ -38,13 +40,13 @@ const Details = () => {
             <Input placeholder="Blvd. Broken Dreams 21" />
           </FormControl>
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>City</FormLabel>
             <Input placeholder="San Francisco" />
           </FormControl>
         </GridItem>
-        <GridItem colSpan={1}>
+        <GridItem colSpan={colSpan}>
           <FormControl>
             <FormLabel>Country</FormLabel>
             <Select>
