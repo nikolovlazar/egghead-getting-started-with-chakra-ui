e0835c14ef095cb22ed0a14d749bca88e02eb3b4
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-09-05 15:58:05 +0200

    lesson 9

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -61,7 +61,7 @@ const Details = () => {
           <Checkbox defaultChecked>Ship to billing address.</Checkbox>
         </GridItem>
         <GridItem colSpan={2}>
-          <Button colorScheme="brand" size="lg" w="full">
+          <Button variant="primary" size="lg" w="full">
             Place Order
           </Button>
         </GridItem>
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -4,6 +4,7 @@ import {
   withDefaultColorScheme,
   withDefaultVariant,
 } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 const inputSelectStyles = {
   variants: {
@@ -21,6 +22,13 @@ const inputSelectStyles = {
         borderRadius: 'none',
       },
     },
+  },
+};
+
+const brandRing = {
+  _focus: {
+    ring: 2,
+    ringColor: 'brand.500',
   },
 };
 
@@ -45,16 +53,31 @@ const theme = extendTheme(
       body: `Inter, ${base.fonts?.body}`,
     },
     components: {
+      Button: {
+        variants: {
+          primary: (props) => ({
+            rounded: 'none',
+            ...brandRing,
+            color: mode('white', 'gray.800')(props),
+            backgroundColor: mode('brand.500', 'brand.200')(props),
+
+            _hover: {
+              backgroundColor: mode('brand.600', 'brand.300')(props),
+            },
+
+            _active: {
+              backgroundColor: mode('brand.700', 'brand.400')(props),
+            },
+          }),
+        },
+      },
       Input: { ...inputSelectStyles },
       Select: { ...inputSelectStyles },
       Checkbox: {
         baseStyle: {
           control: {
             borderRadius: 'none',
-            _focus: {
-              ring: 2,
-              ringColor: 'brand.500',
-            },
+            ...brandRing,
           },
         },
       },
