e1e817661cbffd928656e89d577324a7cccec66f
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-09-05 14:57:46 +0200

    lesson 8

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -5,6 +5,25 @@ import {
   withDefaultVariant,
 } from '@chakra-ui/react';
 
+const inputSelectStyles = {
+  variants: {
+    filled: {
+      field: {
+        _focus: {
+          borderColor: 'brand.500',
+        },
+      },
+    },
+  },
+  sizes: {
+    md: {
+      field: {
+        borderRadius: 'none',
+      },
+    },
+  },
+};
+
 const theme = extendTheme(
   {
     colors: {
@@ -24,6 +43,21 @@ const theme = extendTheme(
     fonts: {
       heading: `Montserrat, ${base.fonts?.heading}`,
       body: `Inter, ${base.fonts?.body}`,
+    },
+    components: {
+      Input: { ...inputSelectStyles },
+      Select: { ...inputSelectStyles },
+      Checkbox: {
+        baseStyle: {
+          control: {
+            borderRadius: 'none',
+            _focus: {
+              ring: 2,
+              ringColor: 'brand.500',
+            },
+          },
+        },
+      },
     },
   },
   withDefaultColorScheme({
