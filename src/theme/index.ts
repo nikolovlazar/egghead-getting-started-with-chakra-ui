fc9d7e8b78f84a11318f479b720314f4dd76d238
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-09-05 13:19:47 +0200

    lesson 7

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,24 +1,39 @@
-import { extendTheme, theme as base } from '@chakra-ui/react';
+import {
+  extendTheme,
+  theme as base,
+  withDefaultColorScheme,
+  withDefaultVariant,
+} from '@chakra-ui/react';
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-      50: '#f5fee5',
-      100: '#e1fbb2',
-      200: '#cdf781',
-      300: '#b8ee56',
-      400: '#a2e032',
-      500: '#8ac919',
-      600: '#71ab09',
-      700: '#578602',
-      800: '#3c5e00',
-      900: '#203300',
+const theme = extendTheme(
+  {
+    colors: {
+      brand: {
+        50: '#f5fee5',
+        100: '#e1fbb2',
+        200: '#cdf781',
+        300: '#b8ee56',
+        400: '#a2e032',
+        500: '#8ac919',
+        600: '#71ab09',
+        700: '#578602',
+        800: '#3c5e00',
+        900: '#203300',
+      },
+    },
+    fonts: {
+      heading: `Montserrat, ${base.fonts?.heading}`,
+      body: `Inter, ${base.fonts?.body}`,
     },
   },
-  fonts: {
-    heading: `Montserrat, ${base.fonts?.heading}`,
-    body: `Inter, ${base.fonts?.body}`,
-  },
-});
+  withDefaultColorScheme({
+    colorScheme: 'brand',
+    components: ['Checkbox'],
+  }),
+  withDefaultVariant({
+    variant: 'filled',
+    components: ['Input', 'Select'],
+  })
+);
 
 export default theme;
